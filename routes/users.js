67468007e9a1c0335859fe791a016cf2318e600f
ra <lilyzahra9@gmail.com>
2021-12-31 10:46:27 +0700

    asm file

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+
+//login handle
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+router.get('/register', (req, res) => {
+    res.render('resgiter')
+});
+
+//register handle
+router.post('/register', (req, res) => {
+});
+router.post('/login', (req, res) => {
+});
+
+//logout
+router.get('/logout', (req, res) => {
+});
+
+module.exports = router;

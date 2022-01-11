9b27758b5716cc7c7f49af220ed420e74fc34985
ra <lilyzahra9@gmail.com>
2022-01-11 11:42:49 +0700

    instll

--- routes/users.js
+++ routes/users.js
@@ -19,4 +19,32 @@ router.post('/login', (req, res) => {
 router.get('/logout', (req, res) => {
 });
 
-module.exports = router;
+const { sign, verify } = require("jsonwebtoken");
+
+const createTokens = (user) => {
+    const accessToken = sign(
+        { username: user.username, id: user.id },
+        "jwtsecretplschange"
+    );
+
+    return accessToken;
+};
+
+const validateToken = (req, res, next) => {
+    const accessToken = req.cookies["access-token"];
+
+    if (!accessToken)
+        return res.status(400).json({ error: "User not Authenticated!" });
+
+    try {
+        const validToken = verify(accessToken, "jwtsecretplschange");
+        if (validToken) {
+            req.authenticated = true;
+            return next();
+        }
+    } catch (err) {
+        return res.status(400).json({ error: err });
+    }
+};
+
+module.exports = { createTokens, validateToken };
